a59396d5029e7487a4d636629f77cc76b54c886b
Neeraj Sirvisetti <neerajsirvisetti@gmail.com>
2022-10-17 17:49:56 +0530

    Added templating of html, unit tests part

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,7 @@
-from django.http import HttpResponse
-def homePageView(request):
-    return HttpResponse("Hello, World!")
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AboutPageView(TemplateView): 
+    template_name = "about.html"
